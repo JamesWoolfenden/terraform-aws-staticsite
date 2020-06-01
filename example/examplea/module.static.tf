93294a674824c9a0ebb9839bd641b91c09bbd076
James Woolfenden <james.woolfenden@slalom.com>
2020-06-01 12:29:32 +0100

    make sure testbuckets dont clash

--- example/examplea/module.static.tf
+++ example/examplea/module.static.tf
@@ -3,5 +3,5 @@ module "static" {
   common_tags         = var.common_tags
   permitted_ip_ranges = var.permitted_ip_ranges
   permitted_iam       = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/*"]
-  s3_bucket_name      = var.s3_bucket_name
+  s3_bucket_name      = "${var.s3_bucket_name}-${data.aws_caller_identity.current.account_id}"
 }
