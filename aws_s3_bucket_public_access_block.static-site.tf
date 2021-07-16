2588feb41c4496bcbeb57f3e354b673934100cd0
James Woolfenden <James.Woolfenden@gmail.com>
2021-07-16 08:05:28 +0100

    add block

---
+++ aws_s3_bucket_public_access_block.static-site.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket_public_access_block" "static-site" {
+  # checkov:skip=CKV_AWS_53: Not appropriate
+  # checkov:skip=CKV_AWS_54: Not appropriate
+  # checkov:skip=CKV_AWS_55: Not appropriate
+  # checkov:skip=CKV_AWS_56: Not appropriate
+  bucket = aws_s3_bucket.static-site.id
+
+  # tfsec:ignore:AWS074
+  block_public_acls   = false
+  # tfsec:ignore:AWS075
+  restrict_public_buckets = false
+  # tfsec:ignore:AWS076
+  block_public_policy = false
+  # tfsec:ignore:AWS073
+  ignore_public_acls  = false
+}
